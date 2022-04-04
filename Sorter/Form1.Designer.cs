b1681bf55725d11a862142f6823c42c574798f2b
Stas Solomon <Solo_jf68@student.itstep.org>
2022-04-04 21:37:31 +0300

    Добавьте файлы проекта.

---
+++ Sorter/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Sorter
+{
+    partial class Sort
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.labelDisplay = new System.Windows.Forms.Label();
+            this.listboxConsole = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.BackColor = System.Drawing.Color.Plum;
+            this.buttonSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSort.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSort.ForeColor = System.Drawing.Color.Indigo;
+            this.buttonSort.Location = new System.Drawing.Point(12, 365);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(344, 73);
+            this.buttonSort.TabIndex = 1;
+            this.buttonSort.Text = "навести порядок";
+            this.buttonSort.UseVisualStyleBackColor = false;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // labelDisplay
+            // 
+            this.labelDisplay.BackColor = System.Drawing.Color.White;
+            this.labelDisplay.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelDisplay.ForeColor = System.Drawing.Color.Crimson;
+            this.labelDisplay.Location = new System.Drawing.Point(12, 9);
+            this.labelDisplay.Name = "labelDisplay";
+            this.labelDisplay.Size = new System.Drawing.Size(344, 72);
+            this.labelDisplay.TabIndex = 2;
+            this.labelDisplay.Text = "НЕ ПРОВЕРЕНО";
+            this.labelDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listboxConsole
+            // 
+            this.listboxConsole.FormattingEnabled = true;
+            this.listboxConsole.ItemHeight = 20;
+            this.listboxConsole.Location = new System.Drawing.Point(12, 98);
+            this.listboxConsole.Name = "listboxConsole";
+            this.listboxConsole.Size = new System.Drawing.Size(344, 244);
+            this.listboxConsole.TabIndex = 3;
+            // 
+            // Sort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(368, 450);
+            this.Controls.Add(this.listboxConsole);
+            this.Controls.Add(this.labelDisplay);
+            this.Controls.Add(this.buttonSort);
+            this.Name = "Sort";
+            this.Text = "Sort";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Label labelDisplay;
+        private System.Windows.Forms.ListBox listboxConsole;
+    }
+}
---
+++ Sorter/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorter
+{
+    public partial class Sort : Form
+    {
+
+        private int num = 0;
+        private string filename = string.Empty;
+
+        private string search_path = "C:\\Users\\KLA\\OneDrive\\Рабочий стол";
+        public string screenshots_path = "C:\\Users\\KLA\\OneDrive\\Рабочий стол\\Images\\Screenshots";
+        private string pictures_path = "C:\\Users\\KLA\\OneDrive\\Рабочий стол\\Images\\Pictures";
+        public Sort()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+
+
+
+            try
+            {
+
+                IEnumerable<string> pics = Directory.EnumerateFiles(search_path);
+
+                foreach (string filename in pics)
+                {
+
+                    Path.GetFileName(filename);
+                    if (filename.Contains(".png"))
+                    {
+                        if (filename.Contains($"Screenshot") || filename.Contains($"Screen"))
+                        {
+
+                            File.Move(filename, Path.Combine(screenshots_path, Path.GetFileName(filename)));
+                            AnalizeFolder();
+
+                        }
+
+                        else
+                        {
+                            File.Move(filename, Path.Combine(pictures_path, Path.GetFileName(filename)));
+                        }
+                    }
+                    else if (filename.Contains(".jpg"))
+                    {
+                        File.Move(filename, Path.Combine(pictures_path, Path.GetFileName(filename)));
+                    }
+
+                }
+
+                labelDisplay.Text = "ГОТОВО";
+                labelDisplay.ForeColor = Color.Green;
+
+            }
+            catch
+            {
+                labelDisplay.Text = "ОШИБКА";
+                MessageBox.Show("Что-то пошло не так! Свяжитесь со Стасом.");
+            }
+        }
+
+
+        void AnalizeFolder()
+        {
+            IEnumerable<string> pics = Directory.EnumerateFiles(screenshots_path);
+            foreach (string filename in pics)
+            {
+                
+                Path.GetFileName(filename);
+
+                FileSystem.RenameFile(filename, ReName());
+
+                listboxConsole.Items.Add(filename);
+
+            }
+        }
+        String ReName()
+        {
+            num++;
+            String name = $"Screen_{num.ToString()}.png";
+            return name;
+        }
+    }
+}
